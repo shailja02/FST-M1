3c2a3d2b37b7808387225a35862753c1cdfd0676
Shailja <90833871+shailja02@users.noreply.github.com>
2023-02-26 14:16:43 +0530

    Add files via upload

---
+++ TestNG/Activities/DemoTwo.java
@@ -0,0 +1,11 @@
+package FST_TestNG;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+public class DemoTwo {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
